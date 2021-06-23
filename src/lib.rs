3a3a13fefa11de0db4469006b2c52a3790991fe5
Yunfei He <i.heyunfei@gmail.com>
2021-06-23 22:10:49 +0800

    feat: impl transformer

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub enum Token {
     ParenRight,
     Name(String),
     Number(String),
+    Str(String),
 }
 
 pub fn tokenizer(input: &str) -> Vec<Token> {
@@ -26,6 +27,21 @@ pub fn tokenizer(input: &str) -> Vec<Token> {
                     current += 1;
                 }
                 tokens.push(Number(num_str));
+                continue;
+            }
+            '"' => {
+                let mut str = String::new();
+                current += 1;
+                while let Some(&c) = input.get(current) {
+                    if c == '"' {
+                        current += 1;
+                        break;
+                    } else {
+                        str.push(c);
+                        current += 1;
+                    }
+                }
+                tokens.push(Str(str));
                 continue;
             }
             'a'..='z' | 'A'..='Z' => {
@@ -56,7 +72,7 @@ pub fn tokenizer(input: &str) -> Vec<Token> {
 pub mod ast {
     #[derive(PartialEq, Eq, Debug)]
     pub struct Program {
-        pub body: Vec<Node>
+        pub body: Vec<Node>,
     }
 
     #[derive(PartialEq, Eq, Debug)]
@@ -66,7 +82,11 @@ pub mod ast {
     }
     #[derive(PartialEq, Eq, Debug)]
     pub struct NumberLiteral {
-        pub value: String
+        pub value: String,
+    }
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct StringLiteral {
+        pub value: String,
     }
 
     #[derive(PartialEq, Eq, Debug)]
@@ -74,23 +94,36 @@ pub mod ast {
         Program(Program),
         CallExpression(CallExpression),
         NumberLiteral(NumberLiteral),
+        StringLiteral(StringLiteral),
     }
 
     impl Node {
         pub fn new_program(body: Vec<Node>) -> Node {
-            Node::Program(Program{ body })
+            Node::Program(Program { body })
         }
         pub fn new_call_expression(name: String, params: Vec<Node>) -> Node {
             Node::CallExpression(CallExpression { name, params })
         }
         pub fn new_number_literal(value: String) -> Node {
-            Node::NumberLiteral(NumberLiteral{ value })
+            Node::NumberLiteral(NumberLiteral { value })
         }
-
+        pub fn new_string_literal(value: String) -> Node {
+            Node::StringLiteral(StringLiteral { value })
+        }
     }
-
+    pub mod visit {
+        pub trait Visitor {
+            fn enter_program(&mut self, _node: &super::Program) {}
+            fn exit_program(&mut self, _node: &super::Program) {}
+            fn enter_call_expression(&mut self, _node: &super::CallExpression, _parent: &super::Node) {}
+            fn exit_call_expression(&mut self, _node: &super::CallExpression, _parent: &super::Node) {}
+            fn enter_number_literal(&mut self, _node: &super::NumberLiteral, _parent: &super::Node) {}
+            fn exit_number_literal(&mut self, _node: &super::NumberLiteral, _parent: &super::Node) {}
+            fn enter_string_literal(&mut self, _node: &super::StringLiteral, _parent: &super::Node) {}
+            fn exit_string_literal(&mut self, _node: &super::StringLiteral, _parent: &super::Node) {}
+        }
+    }
 }
-
 
 pub fn parser(tokens: &Vec<Token>) -> ast::Node {
     use ast::Node;
@@ -99,6 +132,7 @@ pub fn parser(tokens: &Vec<Token>) -> ast::Node {
         let token = &tokens[*current];
         let node = match token {
             Number(value) => Node::new_number_literal(value.clone()),
+            Str(value) => Node::new_string_literal(value.clone()),
             ParenLeft => {
                 *current += 1;
                 let node = if let &Name(ref token_name) = &tokens[*current] {
@@ -134,32 +168,186 @@ pub fn parser(tokens: &Vec<Token>) -> ast::Node {
     program
 }
 
-
-pub trait Visitor {
-    fn enter_program(_n: &mut ast::Program) {
-
+pub fn traverser(ast: &ast::Node, visitor: &mut impl ast::visit::Visitor) {
+    use ast::Node;
+    fn traverser_node(visitor: &mut impl ast::visit::Visitor, node_to_be_match: &Node, parent: Option<&Node>) {
+        match node_to_be_match {
+            n @ Node::Program(_) => {
+                if let Node::Program(p) = n {
+                    visitor.enter_program(p);
+                    let body = &p.body;
+                    traverser_vec(visitor, body, Some(n));
+                    visitor.exit_program(p);
+                }
+            }
+            n @ Node::CallExpression(_) => {
+                if let Node::CallExpression(c) = n {
+                    visitor.enter_call_expression(c, parent.unwrap());
+                    traverser_vec(visitor, &c.params, Some(n));
+                    visitor.exit_call_expression(c, parent.unwrap());
+                }
+            }
+            n @ Node::NumberLiteral(_) => {
+                if let Node::NumberLiteral(v) = n {
+                    visitor.enter_number_literal(v, parent.unwrap());
+                    visitor.exit_number_literal(v, parent.unwrap());
+                }
+            }
+            n @ Node::StringLiteral(_) => {
+                if let Node::StringLiteral(v) = n {
+                    visitor.enter_string_literal(v, parent.unwrap());
+                    visitor.exit_string_literal(v, parent.unwrap());
+                }
+            }
+        }
     }
-    fn leave_program(_n: &mut ast::Program) {
 
+    fn traverser_vec(visitor: &mut impl ast::visit::Visitor, vec: &Vec<Node>, parent: Option<&ast::Node>) {
+        vec.iter()
+            .for_each(|node| traverser_node(visitor, node, parent))
     }
-    fn enter_call_expression(_n: &mut ast::CallExpression) {
+    traverser_node(visitor, ast, None);
+}
 
+pub mod ast_new {
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct Program {
+        pub body: Vec<Node>,
     }
-    fn leave_call_expression(_n: &mut ast::CallExpression) {
-
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct Identifier {
+        pub name: String,
     }
-    fn enter_number_literal(_n: &mut ast::NumberLiteral) {
 
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct CallExpression {
+        pub callee: Identifier,
+        pub arguments: Vec<Node>,
     }
-    fn leave_number_literal(_n: &mut ast::NumberLiteral) {
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct ExpressionStatement {
+        pub expression: CallExpression,
+    }
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct NumberLiteral {
+        pub value: String,
+    }
 
+    #[derive(PartialEq, Eq, Debug)]
+    pub struct StringLiteral {
+        pub value: String,
+    }
+
+    #[derive(PartialEq, Eq, Debug)]
+    pub enum Node {
+        Program(Program),
+        Identifier(Identifier),
+        CallExpression(CallExpression),
+        ExpressionStatement(ExpressionStatement),
+        NumberLiteral(NumberLiteral),
+        StringLiteral(StringLiteral),
+    }
+
+
+    impl Node {
+        pub fn new_program(body: Vec<Node>) -> Node {
+            Node::Program(Program { body })
+        }
+        pub fn new_identifier(name: String) -> Node {
+            Node::Identifier(Identifier { name })
+        }
+        pub fn new_call_expression(callee: Identifier, arguments: Vec<Node>) -> Node {
+            Node::CallExpression(CallExpression {
+                callee,
+                arguments,
+            })
+        }
+        pub fn new_expression_statement(expression: CallExpression) -> Node {
+            Node::ExpressionStatement(ExpressionStatement { expression })
+        }
+        pub fn new_number_literal(value: String) -> Node {
+            Node::NumberLiteral(NumberLiteral { value })
+        }
+        pub fn new_string_literal(value: String) -> Node {
+            Node::StringLiteral(StringLiteral { value })
+        }
+
+        pub fn unwrap_program(self) -> Program {
+            if let  Node::Program(p) = self {
+                p
+            } else {
+                panic!("ops!");
+            }
+        }
     }
 }
 
-pub fn traverser(ast: &ast::Node, visitor: impl Visitor) -> !{
-    panic!("to be impl")
-}
+pub fn transformer(ast: &mut ast::Node) -> ast_new::Program {
+    #[derive(PartialEq, Eq, Debug)]
+    struct State {
+        pub stack: Vec<ast_new::Node>,
+    }
+    impl ast::visit::Visitor for State {
+        fn enter_number_literal(&mut self, node: &ast::NumberLiteral, _parent: &ast::Node) {
+            let new_node = ast_new::Node::new_number_literal(node.value.clone());
+            self.stack.push(new_node);
+        }
+        fn enter_string_literal(&mut self, node: &ast::StringLiteral, _parent: &ast::Node) {
+            let new_node = ast_new::Node::new_string_literal(node.value.clone());
+            self.stack.push(new_node);
+        }
+        fn enter_call_expression(&mut self, node: &ast::CallExpression, parent: &ast::Node) {
+            println!("enter_call_expression _parent: {:?}", parent);
+            let id = ast_new::Identifier { name: node.name.clone() };
+            let exp = ast_new::CallExpression{ callee: id, arguments: vec![], };
+            if let ast::Node::CallExpression(_) = parent {
+                self.stack.push(ast_new::Node::CallExpression(exp));
+            } else {
+                let exp_s = ast_new::Node::new_expression_statement(exp);
+                self.stack.push(exp_s);
+            }
+        }
+        fn enter_program(&mut self, _node: &ast::Program) {
+            let p = ast_new::Node::new_program(vec![]);
+            self.stack.push(p);
+        }
+    }
+    // let mut p = ast_new::Node::new_program(vec![]);
+    let mut s = State { stack: vec![] };
+    // s.stack.push(p);
+    traverser(ast, &mut s);
+    println!("\n\ns.stack: {:?}\n", s.stack);
+    let mut collected = vec![];
+    while let Some(n) = s.stack.pop() {
+        use ast_new::Node;
+        match n {
+            bind @ Node::NumberLiteral(_) => {
+                collected.push(bind);
+            }
+            bind @ Node::StringLiteral(_) => {
+                collected.push(bind);
+            }
+            Node::ExpressionStatement(mut es) => {
+                collected.reverse(); // 由于借助了栈来还原参数，导致参数被逆序了，暂时没有好方法，只能强拧回来
+                es.expression.arguments = collected;
+                collected = vec![Node::ExpressionStatement(es)];
+            }
+            Node::CallExpression(mut c) => {
+                collected.reverse();
+                c.arguments = collected;
+                collected = vec![Node::CallExpression(c)];
+            }
+            Node::Program(mut p) => {
+                collected.reverse();
+                p.body = collected;
+                collected = vec![Node::Program(p)];
+            }
+            _node => panic!("unexpected {:?}", _node)
 
-pub fn transformer() {}
+        }
+    }
+    println!("collected: {:?}\n", collected);
+    collected.pop().unwrap().unwrap_program()
+}
 pub fn code_generator() {}
 pub fn compiler() {}
--- src/test.rs
+++ src/test.rs
@@ -24,16 +24,42 @@ mod tests {
     fn parser_test() {
         let right = {
             use crate::ast::Node;
-            Node::new_program(vec![
-                Node::new_call_expression("add".to_owned(), vec![
+            Node::new_program(vec![Node::new_call_expression(
+                "add".to_owned(),
+                vec![
                     Node::new_number_literal("2".to_string()),
-                    Node::new_call_expression("subtract".to_owned(), vec![
-                        Node::new_number_literal("4".to_owned()),
-                        Node::new_number_literal("2".to_owned()),
-                    ])
-                ]),
-            ])
+                    Node::new_call_expression(
+                        "subtract".to_owned(),
+                        vec![
+                            Node::new_number_literal("4".to_owned()),
+                            Node::new_number_literal("2".to_owned()),
+                        ],
+                    ),
+                ],
+            )])
         };
         assert_eq!(parser(&tokenizer(super::INPUT)), right);
+    }
+    #[test]
+    fn transformer_test() {
+        use crate::ast_new::{Node};
+        let right = ast_new::Program { body: vec![
+            Node::new_expression_statement(ast_new::CallExpression {
+                callee: ast_new::Identifier { name: "add".to_string() },
+                arguments: vec![
+                    Node::new_number_literal("2".to_owned()),
+                    Node::new_call_expression(
+                        ast_new::Identifier { name: "subtract".to_owned() },
+                        vec![
+                            Node::new_number_literal("4".to_owned()),
+                            Node::new_number_literal("2".to_owned()),
+                        ],
+                    ),
+                ],
+            })
+        ] };
+        println!("{:?}", transformer(&mut parser(&tokenizer(super::INPUT))));
+        assert_eq!(transformer(&mut parser(&tokenizer(super::INPUT))), right);
+        // transformer( tokens);
     }
 }
